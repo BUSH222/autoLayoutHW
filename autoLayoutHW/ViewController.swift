3b664c8c4c9784901ac31edb1d1a01c13ed88270
Zahar Litvinchuk <97597336+raynelz@users.noreply.github.com>
2023-08-19 14:59:05 +0300

    Initial Commit

---
+++ autoLayoutHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autoLayoutHW
+//
+//  Created by Захар Литвинчук on 19.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
