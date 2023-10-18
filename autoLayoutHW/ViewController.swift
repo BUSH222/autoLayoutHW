9c8db65beccde8cf8cfcd9b07ca5216e2fe92f4b
Ted V <58144503+BUSH222@users.noreply.github.com>
2023-10-18 22:43:57 +0300

    calculator things - no floats

--- autoLayoutHW/ViewController.swift
+++ autoLayoutHW/ViewController.swift
@@ -8,11 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var firstnum = 0
+    var mode = ""
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        var buttonText = String(sender.title(for: .normal)!)
+        switch buttonText {
+        case "=":
+            break
+        case "+":
+            mode = "+"
+        case "-":
+            mode = "-"
+        case "*":
+            mode = "*"
+        case "/":
+            mode = "/"
+        case "%":
+            // lazy
+            break
+        case ".":
+            // lazy
+            break
+        case "AC":
+            firstnum = 0
+            mode = ""
+        case "+/-":
+            firstnum = -1 * firstnum
+        default:
+            if mode == "" && firstnum != 0 {
+                firstnum = Int(String(firstnum)+buttonText)!
+            } else if mode == "" && firstnum == 0 {
+                firstnum = Int(buttonText)!
+            } else if mode != "" {
+                let stringWithMathematicalOperation: String = String(firstnum)+mode+buttonText
+                let exp: NSExpression = NSExpression(format: stringWithMathematicalOperation)
+                let result = String(format: "%.0f", exp.expressionValue(with:nil, context: nil) as! Double)
+                firstnum = Int(result)!
+            }
+        }
+        mainLabel.text = String(firstnum)
+        
+        // i will not do floats sorry too tired
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
+    
+
 
 
 }
